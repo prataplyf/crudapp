d4ed54ab489e6b430773f257eaf297c696071f40
Ashish Pratap Singh <prataplyf@gmail.com>
2020-12-07 12:06:34 +0530

    db link with ashish

--- crud/config.py
+++ crud/config.py
@@ -1,7 +1,8 @@
 from crud import module as ml
 import pymongo
 # MongoDB
-myclient = pymongo.MongoClient("mongodb+srv://ashishs:bHyoeB2R9CfRXUVb@wsstrategies.k0xsd.mongodb.net/<dbname>?retryWrites=true&w=majority")
+myclient = pymongo.MongoClient("mongodb+srv://prataplyf:ashish12@ashish-hbjy0.mongodb.net/<dbname>?retryWrites=true&w=majority")
+# mongodb+srv://ashishs:bHyoeB2R9CfRXUVb@wsstrategies.k0xsd.mongodb.net
 mydb = myclient.crudapp
 article = mydb.posts # academy funnel
 
